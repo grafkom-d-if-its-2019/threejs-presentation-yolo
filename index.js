558dde8d4b45dcec367a27fbd013c02faa9f6b94
Fandi PJ <37364776+fandipj98@users.noreply.github.com>
2019-12-03 15:08:13 +0700

    point cloud

--- index.js
+++ index.js
@@ -2,6 +2,22 @@
 var scene, camera, renderer;
 
 //Functions
+function createParticles() {
+    var geom = new THREE.Geometry();
+    var material = new THREE.PointCloudMaterial({size: 4, vertexColors: true, color: 0xffffff});
+    for (var x = -5; x < 5; x++) {
+        for (var y = -5; y < 5; y++) {
+            var particle = new THREE.Vector3(x * 10, y * 10, 0);
+            geom.vertices.push(particle);
+            geom.colors.push(new THREE.Color(Math.random() * 0x00ffff));
+        }
+    }
+    var cloud = new THREE.PointCloud(geom, material);
+    scene.add(cloud);
+
+    camera.position.z = 100;
+}
+
 function createSprites()
 {
     var material = new THREE.SpriteMaterial();  
@@ -22,10 +38,12 @@ function spriteTexture()
 {
     var spriteMap = new THREE.TextureLoader().load( "tes.bmp" );
     var spriteMaterial = new THREE.SpriteMaterial( { map: spriteMap, color: 0xffffff } );
+    var spriteMap2 = new THREE.TextureLoader().load( "Spaceship.png" );
+    var spriteMaterial2 = new THREE.SpriteMaterial( { map: spriteMap2, color: 0xffffff } );
     var sprite = new THREE.Sprite( spriteMaterial );
     var sprite2 = sprite.clone();
     sprite2.position.x=3;
-    var sprite3 = new THREE.Sprite( spriteMaterial );
+    var sprite3 = new THREE.Sprite( spriteMaterial2 );
     sprite3.position.x=-3;
     sprite3.copy(sprite2);
     sprite2.position.x=-3;
@@ -52,8 +70,11 @@ function main() {
     // Default Sprites
     // createSprites();
 
+    // Create Particles using PointCloud
+    createParticles();
+
     // Sprite Texture Mapping 
-    spriteTexture();
+    // spriteTexture();
 
     //Render
     animate();
